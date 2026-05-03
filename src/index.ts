a76456fe1f0568b4f0c2087ed7613a1bf517b60e
RiZeE574 <rizee57.jm@gmail.com>
2026-05-04 00:22:24 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { Client, Events, GatewayIntentBits } from "discord.js";
-import { env, assertRequiredEnv } from "./config/env";
+import { env, assertRequiredEnv } from "./config/env.js";
 import { logger } from "./core/logger";
 import { commandMap } from "./commands/index";
 import { handleWelcome, registerAutomations } from "./services/basic.services";
